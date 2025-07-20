56b27fbeb578fdb7373293ef3f583e6e8da41c9c
JaYRaNa213 <tresurejay6@gmail.com>
2025-07-20 23:09:41 +0530

    electron confiregration

--- frontend/electron.js
+++ frontend/electron.js
@@ -1,35 +1,18 @@
 // electron.js
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const isDev = !app.isPackaged;
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
     webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
     },
-    title: "ContextSwap"
   });
 
-  if (isDev) {
-    win.loadURL('http://localhost:5173');
-    win.webContents.openDevTools(); // Optional: remove in production
-  } else {
-    win.loadFile(path.join(__dirname, 'dist', 'index.html'));
-  }
+  // win.loadURL("http://localhost:5173"); // if dev
+  win.loadFile(path.join(__dirname, "dist", "index.html"));
 }
 
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
-});
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
+app.whenReady().then(createWindow);
