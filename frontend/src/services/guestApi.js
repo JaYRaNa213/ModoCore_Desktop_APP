cb306a7a8c679cf366b7628542a3ab1ab53e3c1b
JaYRaNa213 <tresurejay6@gmail.com>
2025-11-17 17:53:04 +0530

    ap

--- frontend/src/services/guestApi.js
+++ frontend/src/services/guestApi.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export const launchGuestTemplateAPI = async (template) => {
-  const res = await axios.post(`${API_BASE}/api/guest/launch`, template);
+  const res = await axios.post(`${API_BASE}/guest/launch`, template);
   return res.data;
 };
